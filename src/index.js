3e8e1f33fdcb9473a2634618bfc07d94556257a7
ALEXEY MALINOVSKIY <87821318+Malins062@users.noreply.github.com>
2022-08-02 17:32:23 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function info() {
+    const m = new Map();
+    m.set('name', 'ajs');
+    m.set('version', '1.0.3');
+    m.set('description', 'Тестовый пакет');
+    return m;
+}
+  
+export default {
+    info 
+}
