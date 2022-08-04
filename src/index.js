dba3aba324b3eac74395d7b4f344ceeaa0c82069
Malins062 <6328673@gmail.com>
2022-08-04 08:40:44 +0300

    New Version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function info() {
+export default function info() {
     const m = new Map();
     m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    m.set('description', 'Тестовый пакет');
+    m.set('version', '1.0.2');
+    m.set('description', 'Тестовый пакет JavaScript');
     return m;
 }
   
-export default {
-    info 
-}
+// export default {
+//     info 
+// }
